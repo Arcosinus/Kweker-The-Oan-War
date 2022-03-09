c8613f200ab80345f83902be98958bdbe6435d91
David <david.roquain@edu.esiee-it.fr>
2022-03-09 12:13:28 +0100

    Description map

--- Assets/Script/CurseurArx.cs
+++ Assets/Script/CurseurArx.cs
@@ -2,15 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class CurseurArx : MonoBehaviour
 {
-    private void OnTriggerStay2D(Collider2D collision)
+    public TextMeshProUGUI info;
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (Input.GetKeyDown(KeyCode.Space)){
+        if (collision.transform.childCount == 1)
+        {
             name = collision.name;
+            info.GetComponent<TextMeshProUGUI>().text = "://" + name + "\n" + collision.transform.GetChild(0).name;
+        }
+        else
+        {
+            name = collision.name;
+            info.GetComponent<TextMeshProUGUI>().text = "://" + name;
+        }
+        if (Input.GetKeyDown(KeyCode.Space)){
             SceneManager.LoadScene(name);
         }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.transform.childCount == 1)
+        {
+            name = collision.name;
+            info.GetComponent<TextMeshProUGUI>().text = "://" + name + "\n" + collision.transform.GetChild(0).name;
+        }
+        else
+        {
+            name = collision.name;
+            info.GetComponent<TextMeshProUGUI>().text = "://" + name;
+        }
+        if (Input.GetKeyDown(KeyCode.Space)){
+            SceneManager.LoadScene(name);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        info.GetComponent<TextMeshProUGUI>().text = "://Analyse";
     }
     private Vector3 vel = Vector3.zero;
     // Start is called before the first frame update
